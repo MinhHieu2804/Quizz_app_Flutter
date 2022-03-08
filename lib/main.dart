ca6bc487a981ef6d053b2f0c9675808a9a23ecbb
Minh Hieu <hieu0985792248@gmail.com>
2022-03-08 16:30:27 +0700

    xu ly logic, end scene

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -17,41 +18,90 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _index = 0;
-  void _answerQuestion() {
+  var totalScore = 0;
+  void _answerQuestion(int score) {
+    totalScore += score;
     setState(() {
       _index += 1;
     });
-    print('Chosen!');
+  }
+
+  void _restart() {
+    totalScore = 0;
+    setState(() {
+      _index = 0;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?',
+      {
+        'questionText': 'What\'s your favorite color?',
+        'answers': [
+          {
+            'text': 'Black',
+            'score': 5,
+          },
+          {
+            'text': 'Blue',
+            'score': 10,
+          },
+          {
+            'text': 'Pink',
+            'score': 15,
+          },
+        ],
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': [
+          {
+            'text': 'Snake',
+            'score': 5,
+          },
+          {
+            'text': 'Dog',
+            'score': 10,
+          },
+          {
+            'text': 'Cat',
+            'score': 15,
+          },
+        ],
+      },
     ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My bar'),
         ),
-        body: Column(
-          children: [
-            Question(questions[_index]),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: _answerQuestion,
-            ),
-          ],
-        ),
+        body: _index < questions.length
+            ? Column(
+                children: [
+                  Question((questions[_index]['questionText']) as String),
+                  ...(questions[_index]['answers'] as List<Map<String, Object>>)
+                      .map((answer) {
+                    return Answer(answer['text'] as String,
+                        () => _answerQuestion(answer['score'] as int));
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Column(children: [
+                  Text(
+                    'You did it!\n' + 'Your score: ' + totalScore.toString(),
+                    style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  FlatButton(
+                    onPressed: _restart,
+                    child: Text('Restart Quiz!'),
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                  )
+                ]),
+              ),
       ),
     );
   }
